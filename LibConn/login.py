968ba827d6d61bd13b00c903f7c9e51612c8a71e
manosmin <95766082+manosmin@users.noreply.github.com>
2022-05-30 22:36:22 +0300

    Update login.py

--- LibConn/login.py
+++ LibConn/login.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import os
-from tkinter import messagebox
-
+from ReturnBook import *
+from LendBook import *
 
 def login_success():
     global screen3
@@ -92,7 +92,7 @@ def register():
     password_entry = Entry(screen1, textvariable=password)
     password_entry.pack()
     Label(screen1, text="").pack()
-    Button(screen1, text="Υποβολή", font=('Courier', 12), fg='#d68227',bg='white', width=10, height=1, command='').pack()
+    Button(screen1, text="Υποβολή", font=('Courier', 12), fg='#d68227',bg='white', width=10, height=1, command=home_screen).pack()
 
 
 def login():
@@ -121,7 +121,7 @@ def login():
     password_entry1 = Entry(screen2, textvariable=password_verify)
     password_entry1.pack()
     Label(screen2, text="").pack()
-    Button(screen2, text="Υποβολή", font=('Courier', 12), fg='#d68227',bg='white', width=10, height=1, command='').pack()
+    Button(screen2, text="Υποβολή", font=('Courier', 12), fg='#d68227',bg='white', width=10, height=1, command=home_screen).pack()
 
 
 def main_screen():
@@ -143,6 +143,72 @@ def main_screen():
            fg='#d68227', font=('Courier', 12), command=register).pack()
 
     screen.mainloop()
+
+def home_screen():
+
+    global screen6, Canvas1, icon1, icon2, icon3, icon4, icon5, btn1, btn2, btn3, btn4, btn5, btn7, btn8, btn9, headingFrame1, headingLabel1
+
+    screen6 = Toplevel(screen)
+    screen6.title("LibConn")
+    screen6.geometry("600x500")
+    screen6.resizable(width=0, height=0)
+
+    Canvas1 = Canvas(screen6)
+    Canvas1.create_image(300, 340)
+    Canvas1.config(bg="white")
+    Canvas1.pack(expand=True, fill=BOTH)
+
+    icon1 = PhotoImage(file='img/home.png')
+    icon2 = PhotoImage(file='img/profile.png')
+    icon3 = PhotoImage(file='img/calendar.png')
+    icon4 = PhotoImage(file='img/bookcase.png')
+    icon5 = PhotoImage(file='img/wallet.png')
+
+    btn7 = Button(screen6, image=icon1, bg='white', fg='#d68227',
+                command='')
+    btn7.place(relx=0.16, rely=0.12, relwidth=0.12, relheight=0.12)
+
+    btn8 = Button(screen6, image=icon2, bg='white', fg='#d68227',
+                command='')
+    btn8.place(relx=0.74, rely=0.12, relwidth=0.12, relheight=0.12)
+
+    headingFrame1 = Frame(screen6, bg="#d68227", bd=2)
+    headingFrame1.place(relx=0.3, rely=0.12, relwidth=0.42, relheight=0.12)
+
+    headingLabel1 = Label(headingFrame1, text="LibConn", bg='white',
+                        fg='#d68227', font=('Courier', 28, 'bold'))
+    headingLabel1.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+
+    btn9 = Button(screen6, image=icon3, bg='white', fg='#d68227', command='')
+    btn9.place(relx=0.3, rely=0.3, relwidth=0.12, relheight=0.12)
+
+    btn10 = Button(screen6, image=icon4, bg='white', fg='#d68227', command='')
+    btn10.place(relx=0.45, rely=0.3, relwidth=0.12, relheight=0.12)
+
+    btn11 = Button(screen6, image=icon5, bg='white', fg='#d68227', command='')
+    btn11.place(relx=0.6, rely=0.3, relwidth=0.12, relheight=0.12)
+
+    btn1 = Button(screen6, text="Δανεισμός", bg='white',
+                fg='#d68227', font=('Courier', 12), command=lendBook)
+    btn1.place(relx=0.3, rely=0.45, relwidth=0.42, relheight=0.08)
+
+    btn2 = Button(screen6, text="Επιστροφή", bg='white',
+                fg='#d68227', font=('Courier', 12), command=returnBook)
+    btn2.place(relx=0.3, rely=0.55, relwidth=0.42, relheight=0.08)
+
+    btn3 = Button(screen6, text="Προτεινόμενα", bg='white',
+                fg='#d68227', font=('Courier', 12), command='')
+    btn3.place(relx=0.3, rely=0.65, relwidth=0.42, relheight=0.08)
+
+    btn4 = Button(screen6, text="Αξιολόγηση", bg='white',
+                fg='#d68227', font=('Courier', 12), command='')
+    btn4.place(relx=0.3, rely=0.75, relwidth=0.42, relheight=0.08)
+
+    btn5 = Button(screen6, text="Βοήθεια", bg='white',
+                fg='#d68227', font=('Courier', 12), command='')
+    btn5.place(relx=0.3, rely=0.85, relwidth=0.42, relheight=0.08)
+
 
 
 main_screen()
